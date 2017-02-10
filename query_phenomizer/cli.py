21c0102f26e37ed781474461829c223f3975c146
Måns Magnusson <mans.magnusson@scilifelab.se>
2017-02-10 10:22:05 +0100

    Changes output from query_phenomizer
    Before there was a misunderstanding so there where only some genes reported.
    The output is now the diseases associated with the phenotypes that are used with p-values and genes

--- query_phenomizer/cli.py
+++ query_phenomizer/cli.py
@@ -26,9 +26,8 @@ def test_args(*args):
         print(arg)
 
 @click.command()
-@click.option('-t', '--hpo_term',
-        multiple=True,
-        help="Give hpo terms either on the form 'HP:0001623', or '0001623'"
+@click.argument('hpo_term',
+        nargs=-1,
 )
 @click.option('-u', '--username',
         type=str,
@@ -47,27 +46,32 @@ def test_args(*args):
         help="Specify the path to a file for storing the phenomizer output."
 )
 @click.option('--p_value_limit',
-        nargs=1,
         default=1.0,
         show_default=True,
         help='Specify the highest p-value that you want included.'
+)
+@click.option("--to-json",
+        is_flag=True,
+        help="If result should be printed to json format"
 )
 @click.option('-v', '--verbose',
         count=True,
         default=2
 )
 @click.pass_context
-def cli(ctx, hpo_term, check_terms, output, p_value_limit, verbose, username, password):
+def cli(ctx, hpo_term, check_terms, output, p_value_limit, verbose, username, 
+        password, to_json):
+    "Give hpo terms either on the form 'HP:0001623', or '0001623'"    
     loglevel = LEVELS.get(min(verbose, 3))
     configure_stream(level=loglevel)
     
     if not hpo_term:
-        logger.warning("Please specify at least one hpo term with '-t/--hpo_term'.")
+        logger.info("Please specify at least one hpo term with '-t/--hpo_term'.")
         ctx.abort()
 
     if not (username and password):
-        logger.warning("Please a username and a password.")
-        logger.warning("Contact sebastian.koehler@charite.de.")
+        logger.info("Please a username and a password.")
+        logger.info("Contact sebastian.koehler@charite.de.")
         ctx.abort()
     
     hpo_list = []
@@ -84,24 +88,38 @@ def cli(ctx, hpo_term, check_terms, output, p_value_limit, verbose, username, pa
                 logger.info("HPO term : {0} does not exist".format(term))
             else:
                 logger.info("HPO term : {0} does exist!".format(term))
-    
+        ctx.abort()
     else:
+        click.echo("{0}\t{1}\t{2}\t{3}".format(
+            'p_value', 'disease', 'description', 'genes'
+        ))
         try:
-            results = query(username, password, hpo_list)
+            for result in query(username, password, *hpo_list):
+                if to_json:
+                    click.echo(json.dumps(result))
+                else:
+                    click.echo("{0}\t{1}:{2}\t{3}\t{4}".format(
+                        result['p_value'], 
+                        result['disease_source'],
+                        result['disease_nr'],
+                        result['description'],
+                        ','.join(result['gene_symbols'])
+                    ))
+                
         except RuntimeError as e:
             logger.error(e.message)
             ctx.abort()
         
-        nr_significant_genes = 0
-        for result in results:
-            if result['p_value'] < p_value_limit:
-                nr_significant_genes += 1
-                if output:
-                    output.write(result['raw_line'] + '\n')
-                else:
-                    print(json.dumps(result))
-        
-        if nr_significant_genes == 0:
-            logger.info("There where no significant genes with p value"\
-                        " < {0}".format(p_value_limit))
+        # nr_significant_genes = 0
+        # for result in results:
+        #     if result['p_value'] < p_value_limit:
+        #         nr_significant_genes += 1
+        #         if output:
+        #             output.write(result['raw_line'] + '\n')
+        #         else:
+        #             print(json.dumps(result))
+        #
+        # if nr_significant_genes == 0:
+        #     logger.info("There where no significant genes with p value"\
+        #                 " < {0}".format(p_value_limit))
 
